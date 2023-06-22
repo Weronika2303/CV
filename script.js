198b0fce98ebcf50617a01f372878454f6f409fc
Weronika <worsztynowicz.weronika@gmail.com>
2023-06-22 23:59:45 +0200

    Add new files

---
+++ script.js
@@ -0,0 +1,10 @@
+function redirectToURL(type) {
+	switch (type) {
+		case 'linkedin':
+			window.open('https://www.linkedin.com/in/weronika-w-1492201bb/', '_blank')
+			break
+		case 'github':
+			window.open('https://www.google.com', '_blank')
+			break
+	}
+}
